bef714b81d19c1a331dd14023427f8a8e458303c
4stra <esterduarte99@gmail.com>
2023-11-21 18:01:37 -0300

    Add files via upload

---
+++ deteccao_facial_haar_cascade.py
@@ -0,0 +1,13 @@
+import cv2
+
+imagem = cv2.imread("image.jpg") #caminho para a imagem
+deteccao = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml") #caminho para o algoritmo
+imagemPB = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY) #conversao da imagem para preto e branco
+face = deteccao.detectMultiScale(imagemPB, 1.3, 3)
+
+for (x, y, largura, altura) in face:
+    retangulo = cv2.rectangle(imagem, (x, y), (x + largura, y + altura), (0, 255, 0), 3)
+    
+cv2.imshow('', imagem)
+cv2.waitKey(0)
+
